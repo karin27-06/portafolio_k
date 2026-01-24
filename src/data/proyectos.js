d6b9517afa4dd8f15a7679087993740c3bda52c6
karin27-06 <karinsntmr@gmail.com>
2026-01-23 22:30:57 -0500

    proyectos

---
+++ src/data/proyectos.js
@@ -0,0 +1,122 @@
+export const proyectos = [
+  //RESTAURANTE
+  {
+    slug: "restaurante",
+    titulo: "Sistema de ventas para restaurantes",
+    descripcionCorta: "Sistema para restaurantes con registro y facturacion electronica",
+    descripcionCompleta:
+      "Este proyecto es una plataforma de ventas para restaurantes, desarrollada para ofrecer una experiencia de registro moderna, rápida y segura. Incluye autenticación de usuarios, gestión de productos, platos y pasarela de pagos integrada.",
+    fecha: "2025-04-01",
+    tecnologias: ["React", "Node.js", "MongoDB"],
+    imagenes: [
+      "/images/proyectos/restcapt.png",
+      "/images/proyectos/restcapt2.png",
+    ],
+    github: "https://github.com/tuusuario/ecommerce-react",
+    demo: "https://ecommerce-demo.vercel.app",
+    video: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+    caracteristicas: [
+      "Autenticación de usuarios",
+      "Roles y permisos para cierto modulos",
+      "Pasarela de pagos",
+      "Panel de administración",
+      "Dashboard dinamico",
+      "Reservacion con notificaciones ingredas",
+      "Envio de correos y mensajes hacia Email y WhatsApp",
+    ],
+  },
+
+  //MOTORES
+  {
+    slug: "motores",
+    titulo: "Sistema de gestión de motores para Talleres y Centros Mecánicos",
+    descripcionCorta: "Gestión de motores de agua y terrestres para brindar el registro y mantenimiento de los mismos",
+    descripcionCompleta:
+      "Sistema  moderno enfocado en la administración de registro, mantenimiento, limpieza y control de motores, diseñada para tecnicos mecanicos.",
+    fecha: "2025-11-05",
+    tecnologias: ["Vue.js", "Firebase"],
+    imagenes: [
+      "/images/proyectos/motorcapt.png",
+      "/images/proyectos/motorcapt2.png",
+    ],
+    github: "https://github.com/tuusuario/app-tareas",
+    demo: "https://app-tareas.web.app",
+    video: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+    caracteristicas: [
+      "Login con Google",
+      "CRUD de tareas",
+      "Persistencia en la nube",
+    ],
+  },
+
+  //SISTEMA FACIAL
+  {
+    slug: "sis-facial",
+    titulo: "Sistema de Reconocimiento facial y Dactical",
+    descripcionCorta: "Sistema moderno enfocado en el reconocimiento biometrico facial y dacticalr para la gestión de espacios de trabajo",
+    descripcionCompleta:
+      "Solución inteligente para la identificacion segura, rapida y confiable hecha con Laravel y Google cloud. Ideal para empresas, instituciones y sistemas de control de acceso modernos para cualquier espacio de trabajo",
+    fecha: "2025-06-12",
+    tecnologias: ["Vue.js", "Firebase"],
+    imagenes: [
+      "/images/proyectos/facialcapt.png",
+      "/images/proyectos/facialcapt2.png",
+    ],
+    github: "https://github.com/tuusuario/app-tareas",
+    demo: "https://app-tareas.web.app",
+    video: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+    caracteristicas: [
+      "Login con Google",
+      "CRUD de tareas",
+      "Persistencia en la nube",
+    ],
+  },
+
+  //SISTEMA SOLIDARIA
+  {
+    slug: "solidaria",
+    titulo: "Sistema para Farmacias Solidaria",
+    descripcionCorta: "Sistema enfocado en el registro de productos farmaceuticos y atencion personalizada en base a la salud del cliente",
+    descripcionCompleta:
+      "Aplicación sistematica enfocada en el registro y venta de farmaceuticos con facturacion electronica, ofreciendo productos accesibles y un firme compromiso con la salud",
+    fecha: "2025-05-10",
+    tecnologias: ["Vue.js", "Firebase"],
+    imagenes: [
+      "/images/proyectos/farmsolidcapt.png",
+      "/images/proyectos/farmsolidcapt2.png",
+      "/images/proyectos/farmsolidcapt3.png",
+    ],
+    github: "https://github.com/tuusuario/app-tareas",
+    demo: "https://app-tareas.web.app",
+    video: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+    caracteristicas: [
+      "Login con Google",
+      "CRUD de tareas",
+      "Persistencia en la nube",
+    ],
+  },
+
+  //ADMINISTRACION
+  {
+    slug: "administracion",
+    titulo: "Sistema de administración de productos tecnologicos",
+    descripcionCorta: "Aplicación encargada del registro de servicios tecnologicos exclusivos de desarrollo.",
+    descripcionCompleta:
+      "Aplicación enfocada en la productividad personal, permite crear, editar y eliminar tareas con autenticación segura usando Firebase.",
+    fecha: "2024-05-02",
+    tecnologias: ["Vue.js", "Firebase"],
+    imagenes: [
+      "/images/proyectos/adminiscapt.png",
+      "/images/proyectos/adminiscapt2.png",
+      "/images/proyectos/adminiscapt3.png",
+    ],
+    github: "https://github.com/tuusuario/app-tareas",
+    demo: "https://app-tareas.web.app",
+    video: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+    caracteristicas: [
+      "Login con Google",
+      "CRUD de tareas",
+      "Persistencia en la nube",
+    ],
+  },
+];
